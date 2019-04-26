12f421eae12795c8fc2f4e5517e4c298b25cee04
leechang0423 <leechang0423@naver.com>
2019-04-26 14:20:57 +0900

    javascript join() 쓰도록 변경

--- js/Array1.js
+++ js/Array1.js
@@ -6,21 +6,25 @@ function assertEquals(a, b) {
 
 const number = 123;
 
-function execute(number,numbers) {
+function execute(number, numbers) {
     let str = numbers;
     if (number < 1) {
-        const resultString = str.reverse().join();
+        const resultString = str.reverse().join("");
         return resultString;
     }
+
     const digit = number % 10;
     const remainNumber = Math.floor(number / 10);
     str.push(digit);
-    execute(remainNumber,str);
+    return execute(remainNumber, str);
 
 }
 
 function number2String(number) {
-    return execute(number,[]);
+    return execute(number, []);
 }
 
 assertEquals(number2String(number), "123");
+
+
+
