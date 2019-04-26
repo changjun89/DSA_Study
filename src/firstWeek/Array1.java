5dc2846c83c27887074fa68b4bb1f0b49b47a45f
leechang0423 <leechang0423@naver.com>
2019-04-26 09:30:50 +0900

    1번문제 while문으로 풀이 . list로 닮아서 풀었다는게.. 좀

--- src/firstWeek/Array1.java
+++ src/firstWeek/Array1.java
@@ -9,6 +9,29 @@ public class Array1 {
 
 
     public String int2Str(int input) {
-        return "123";
+        if(input ==0 ) {
+            return 0+"";
+        }
+        return digitString(digitLIst(Math.abs(input)));
+    }
+
+    public List<Integer> digitLIst(int input) {
+        List<Integer> list = new ArrayList<>();
+        int remainder = input;
+
+        while(remainder > 0 ) {
+            int num = remainder%10;
+            remainder = remainder/10;
+            list.add(num);
+        }
+        return list;
+    }
+
+    public String digitString(List<Integer> list) {
+        String digitString = "";
+        for(int i =list.size()-1 ;  i >= 0 ; i--) {
+            digitString += (list.get(i)+"");
+        }
+        return digitString;
     }
 }
--- test/firstWeek/Array1Test.java
+++ test/firstWeek/Array1Test.java
@@ -4,6 +4,9 @@ package firstWeek;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class Array1Test {
@@ -17,9 +20,21 @@ public class Array1Test {
 
     @Test
     public void int2StrTest() {
-        int input =123;
-        assertEquals(array1.int2Str(input),"123");
+        //assertEquals(array1.int2Str(123),"123");
+        assertEquals(array1.int2Str(12), "12");
+        assertEquals(array1.int2Str(0), "0");
+        assertEquals(array1.int2Str(-123), "123");
+    }
 
+    @Test
+    public void digitLIstTest() {
+        int input = 123;
+        List<Integer> result = new ArrayList<>();
+        result.add(3);
+        result.add(2);
+        result.add(1);
+        assertEquals(result, array1.digitLIst(input));
     }
 
 }
+
