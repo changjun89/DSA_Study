83d3a426785dd04d4b9b4542378a7f995ae9fdee
leechang0423 <leechang0423@naver.com>
2019-05-02 17:34:31 +0900

    리뷰내용 반영해서 수정

--- src/firstWeek/ReverseSentence.java
+++ src/firstWeek/ReverseSentence.java
@@ -1,7 +1,7 @@
 package firstWeek;
 
 public class ReverseSentence {
-    private static final String SEPARATOR=" ";
+    private static final String SEPARATOR = " ";
     private String reverseStr = "";
     private String sentence;
 
@@ -9,29 +9,46 @@ public class ReverseSentence {
         this.sentence = sentence;
     }
 
-    private int blankIdx() {
+    private int blankIndex() {
         return sentence.indexOf(SEPARATOR);
     }
+
     private boolean hasSeparator() {
-        return sentence.length() > 0 && sentence.indexOf(SEPARATOR) > -1;
+        return sentence.length() > 0 && blankIndex() > -1;
+    }
+
+    private boolean hasSeparator(int blankIndex) {
+        return sentence.length() > 0 && blankIndex > -1;
     }
 
     private String firstWord() {
-        return sentence.substring(0, blankIdx());
+        return sentence.substring(0, blankIndex());
     }
+
+    private String firstWord(int blankIndex) {
+        return sentence.substring(0, blankIndex);
+    }
+
     private String removeFirstWord() {
-        return sentence.substring(blankIdx()+1);
+        return sentence.substring(blankIndex() + 1);
     }
+
+    private String removeFirstWord(int blankIndex) {
+        return sentence.substring(blankIndex + 1);
+    }
+
     private String delLastSeparator() {
         return reverseStr.substring(0, reverseStr.length() - 1);
     }
 
     public String reverse() {
-        while(hasSeparator()){
-            reverseStr =firstWord()+SEPARATOR+reverseStr;
-            sentence = removeFirstWord();
+        int blankIndex = blankIndex();
+        while (hasSeparator(blankIndex)) {
+            reverseStr = firstWord(blankIndex) + SEPARATOR + reverseStr;
+            sentence = removeFirstWord(blankIndex);
+            blankIndex = blankIndex();
         }
-        reverseStr = sentence +SEPARATOR+reverseStr;
+        reverseStr = sentence + SEPARATOR + reverseStr;
         return delLastSeparator();
     }
 
