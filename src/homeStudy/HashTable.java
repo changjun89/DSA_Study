7cd71fd60f577c517cac3bde74d5bef5583ce5bc
changjun89 <leechang0423@naver.com>
2019-06-13 00:00:36 +0900

    get 구현

--- src/homeStudy/HashTable.java
+++ src/homeStudy/HashTable.java
@@ -1,6 +1,7 @@
 package homeStudy;
 
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class HashTable {
@@ -12,6 +13,10 @@ public class HashTable {
     table.put("leechangjun", "leechangjun@naver.com");
     table.put("changjun", "changjun@naver.com");
 
+    Object result = table.get("leechangjun");
+    Object result2 = table.get("changjun");
+    System.out.println(result.toString());
+    System.out.println(result2.toString());
   }
 
   private int loseHashCode(Object key) {
@@ -36,6 +41,20 @@ public class HashTable {
     nodes.add(new Node(key, value));
   }
 
+  public Object get(Object key) {
+    int position = loseHashCode(key);
+    if (table[position] != null) {
+      LinkedList<Node> nodes = (LinkedList) table[position];
+      Iterator<Node> iterator = nodes.iterator();
+      while (iterator.hasNext()) {
+        Node node = iterator.next();
+        if (node.getKey().equals(key)) {
+          return node.getValue();
+        }
+      }
+    }
+    return null;
+  }
 
   public void remove(Object key) {
     table[loseHashCode(key)] = null;
--- src/homeStudy/Node.java
+++ src/homeStudy/Node.java
@@ -10,6 +10,21 @@ public class Node {
     this.value = value;
   }
 
+  public Object getKey() {
+    return key;
+  }
+
+  public void setKey(Object key) {
+    this.key = key;
+  }
+
+  public Object getValue() {
+    return value;
+  }
+
+  public void setValue(Object value) {
+    this.value = value;
+  }
 
   @Override
   public String toString() {
